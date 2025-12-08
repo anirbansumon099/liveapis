a5f1992edbe0830e5ef72a508224c010dc21ea93
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-12-08 10:52:57 +0600

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -26,7 +26,7 @@ router.get("/users", (req, res) => {
 router.get("/ayna",async(req,res)=>{
     try {
         const postData = {  };
-        const result = await makeAuthPostRequest_ayna(postData);
+        const result = await makeAuthPostRequest_2("",postData);
 
         if (result.success) {
             res.json({
