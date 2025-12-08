11058c155a46dddb82f9c839ab9946d48de28410
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-12-08 08:23:52 +0600

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const { URL } = require("url");
+
 const auth = require("../components/middleware/external-auth");
-const {makeAuthPostRequest} = require('../components/roarzone');
 const router = express.Router();
 let users = [
   { id: 1, name: "Anirban" },
@@ -9,25 +8,11 @@ let users = [
 ];
 router.use(auth);
 // GET /api/users
-router.get("/users", async (req, res) => {
-  try {
-        const postData = {  };
-        const result = await makeAuthPostRequest(postData);
-
-        if (result.success) {
-            console.log("✅ Server Response:", result.data);
-            res.send(result.data);
-        } else {
-            console.error("❌ Error:", result.error);
-            res.status(500).send(result.error);
-        }
-    } catch (err) {
-        console.error("[ERROR] /token route:", err.message);
-        res.status(500).send("Server Error");
-    }  
-    
-    
-}
+router.get("/users", (req, res) => {
+  res.json({
+    success: true,
+    user
+});
 
 // POST /api/users
 router.post("/roarzone", (req, res) => {
