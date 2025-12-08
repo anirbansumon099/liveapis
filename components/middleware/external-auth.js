856426b94e482479c5c3908eecbbac95c80e5d9a
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-12-08 08:31:32 +0600

    update external-auth.js

--- components/middleware/external-auth.js
+++ components/middleware/external-auth.js
@@ -1,26 +1,23 @@
 module.exports = function (req, res, next) {
     const apiKey = req.headers["x-api-key"];
     const clientId = req.headers["x-client-id"];
-    
 
     // তোমার আসল secret
-    const VALID_API_KEY = "fuck_your_system"; 
-    const VALID_CLIENT_ID = "streamarpro";
+    const VALID_API_KEY = "1234567890"; 
+    const VALID_CLIENT_ID = "myapp";
 
     if (!apiKey || !clientId) {
-        return res.status(503);
-        /*json({
+        return res.status(401).json({
             success: false,
             message: "Missing required headers"
-        });*/
+        });
     }
 
     if (apiKey !== VALID_API_KEY || clientId !== VALID_CLIENT_ID) {
-        return res.status(503);
-        /*json({
+        return res.status(401).json({
             success: false,
             message: "Invalid authentication headers"
-        });*/
+        });
     }
 
     // সব ঠিক থাকলে → API চলবে
