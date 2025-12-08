dfe6a65d4647f0551a8a97674a22a6d2caa2619e
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-12-08 08:55:11 +0600

    update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 // Set EJS template folder
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use("/", require("./routes/web"));
