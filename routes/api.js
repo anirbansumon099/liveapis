9651122aba120617473c23a089d57488983a62e4
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-12-08 08:42:31 +0600

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,28 @@ router.get("/users", (req, res) => {
   });
 });
 
+
+app.get("/token", async (req, res) => {
+    try {
+        const postData = {  };
+        const result = await makeAuthPostRequest(postData);
+
+        if (result.success) {
+            console.log("✅ Server Response:", result.data);
+            res.send(result.data);
+        } else {
+            console.error("❌ Error:", result.error);
+            res.status(500).send(result.error);
+        }
+    } catch (err) {
+        console.error("[ERROR] /token route:", err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+
+
 // POST /api/users
 router.post("/users", (req, res) => {
   const { name } = req.body;
